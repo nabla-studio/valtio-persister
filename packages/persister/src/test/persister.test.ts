e0065b0d1115987edeff399afcedb4923c821097
Davide Segullo <davidesegullo@gmail.com>
2024-04-23 12:08:32 +0200

    test(persister): :white_check_mark: update test
    updated tests for not being tied to react.

--- packages/persister/src/test/persister.test.ts
+++ packages/persister/src/test/persister.test.ts
@@ -1,7 +1,4 @@
-import { useSnapshot } from 'valtio';
-import { fireEvent, render } from '@testing-library/react';
 import { proxyWithPersister } from '../persister';
-import * as React from 'react';
 
 const initialState = {
   counter: 0,
@@ -14,60 +11,34 @@ describe('proxyWithPersister', () => {
       name: persisterName,
     });
 
-    const Counter = () => {
-      const snap = useSnapshot(store);
+    ++store.counter;
 
-      return (
-        <>
-          <div>count: {snap.counter}</div>
-          <button
-            onClick={() => {
-              ++store.counter;
-            }}
-          >
-            button
-          </button>
-        </>
-      );
-    };
+    await Promise.resolve();
 
-    const { getByText, findByText, unmount } = render(
-      <React.StrictMode>
-        <Counter />
-      </React.StrictMode>
+    expect(store).toStrictEqual({ counter: 1 });
+    expect(localStorage.getItem(persisterName)).toStrictEqual(
+      JSON.stringify({
+        state: { counter: 1 },
+        version: 0,
+      })
     );
-
-    await findByText('count: 0');
-
-    fireEvent.click(getByText('button'));
-    await findByText('count: 1');
-
-    unmount();
+    expect(localStorage.length).toBe(1);
   });
   test('proxyWithPersister should hydrate data from localStorage', async () => {
     const { store } = proxyWithPersister(initialState, {
       name: persisterName,
     });
 
-    const Counter = () => {
-      const snap = useSnapshot(store);
+    await Promise.resolve();
 
-      return (
-        <>
-          <div>count: {snap.counter}</div>
-        </>
-      );
-    };
-
-    const { findByText, unmount } = render(
-      <React.StrictMode>
-        <Counter />
-      </React.StrictMode>
+    expect(store).toStrictEqual({ counter: 1 });
+    expect(localStorage.getItem(persisterName)).toStrictEqual(
+      JSON.stringify({
+        state: { counter: 1 },
+        version: 0,
+      })
     );
-
-    await findByText('count: 1');
-
-    unmount();
+    expect(localStorage.length).toBe(1);
   });
   test('proxyWithPersister should not persist excluded keys to localStorage', async () => {
     const excludedKeys = ['excludedKey'];
